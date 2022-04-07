914c2b6c269559a5bf17f4fffcdc801f80efb21a
Peter Ihimire <peterihimire@gmail.com>
2022-04-07 11:09:45 +0100

    Fixed ssr issue with the homepage carousel and countdown timer, by creating a HOC called no-ssr-wrapper to wrap and disable ssr

---
+++ hoc/noSSRWrapper/index.js
@@ -0,0 +1,7 @@
+import dynamic from "next/dynamic";
+import React from "react";
+
+const NoSSRWrapper = (props) => {
+  return <React.Fragment>{props.children}</React.Fragment>;
+};
+export default dynamic(() => Promise.resolve(NoSSRWrapper), { ssr: false });
